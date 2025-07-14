6f9e59395e05dfdd723556739b36a7bddffed96c
Juan Pablo <120145816+Dev-JuanPa@users.noreply.github.com>
2025-07-14 21:43:21 +0000

    Creating users using insomnia

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,11 +5,27 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_bcrypt import Bcrypt
 
 api = Blueprint('api', __name__)
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 # Allow CORS requests to this API
 CORS(api)
+
+
+@api.route('/register', methods=['POST'])
+def register_user():
+    body = request.get_json()
+    new_user = User(email=body["email"], fullname=body["fullname"])
+    # Password here
+    hashed_password = bcrypt.generate_password_hash(
+        body["password"]).decode("utf-8")
+    new_user.password = hashed_password
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.serialize()), 201
 
 
 @api.route('/hello', methods=['POST', 'GET'])
